88662fea607ac8def01c7edd2fe1e828ee340d14
MihailP <mihailpascu21@gmail.com>
2021-11-05 03:16:58 +0200

    Implemented HelloWorld

---
+++ TheCProgrammingLanguage/_001_helloWorld.c
@@ -0,0 +1,24 @@
+/******************************************************************
+ * \file	_001_helloWorld.c
+ *
+ * \brief 	Program that prints "hello, world" on the screen.
+ * 
+ * \description	This file is using the stdio library to print 
+ * 		on screen the string "hello, world".
+ *
+ * \author	Mihail P. (MP)
+ * \date	05.XI.2021 02:49AM
+ * \tag		"The C Programming Language 2nd Edition"
+ * 		by Brian W. Kernighan and Dennis M. Ritchie
+ * 		Chapter 1, Section 1.1
+ * 		page 6, Example Code
+ ******************************************************************/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("hello, world\n");
+	return 0;
+}
