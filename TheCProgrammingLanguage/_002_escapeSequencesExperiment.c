54b5341959a7ef4eadf835eca902cbc4447e49ca
MihailP <mihailpascu21@gmail.com>
2021-11-07 03:49:41 +0200

    Solved Exercise1-2

---
+++ TheCProgrammingLanguage/_002_escapeSequencesExperiment.c
@@ -0,0 +1,52 @@
+/******************************************************************
+ * \file	_002_escapeSequencesExperiment.c
+ *
+ * \brief 	Program that experiments with Escape Sequences.
+ * 
+ * \description	This file is using the stdio library to print 
+ * 		on screen the Escape Sequences
+ *
+ * \author	Mihail P. (MP)
+ * \date	07.XI.2021 02:47AM
+ * \tag		"The C Programming Language 2nd Edition"
+ * 		by Brian W. Kernighan and Dennis M. Ritchie
+ * 		Chapter 1, Section 1.1
+ * 		page 8, Exercise 1-2
+ ******************************************************************/
+
+
+#include <stdio.h>
+
+struct EscapeSequence
+{
+ char string[3];
+ char value;
+};
+
+typedef struct EscapeSequence escapeSequence;
+
+int main(void)
+{
+	escapeSequence array[15]=
+	{
+		{"\\n",'\n'},			
+		{"\\t",'\t'},			
+		{"\\a",'\a'},			
+		{"\\r",'\r'},			
+		{"\\\\",'\\'},			
+		{"\\'",'\''},			
+		{"\\\"",'\"'},			
+		{"\\0",'\0'},			
+		{"\\?",'\?'},			
+		{"\\v",'\v'},			
+		{"\\b",'\b'},			
+		{"\\e",'\e'},			
+		{"\\f",'\f'}
+	};
+
+	for(int i=0; i<13; i++)
+	{
+		printf("Character %s is printed as %c.\n",array[i].string, array[i].value);
+	}
+	return 0;
+}
