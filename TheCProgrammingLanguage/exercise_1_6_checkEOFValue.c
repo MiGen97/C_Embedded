6018591d8c3d52b61543e6fa89f22625c62d9eed
MihailP <mihailpascu21@gmail.com>
2021-11-09 03:36:03 +0200

    Solution for Exercise 1-6

---
+++ TheCProgrammingLanguage/exercise_1_6_checkEOFValue.c
@@ -0,0 +1,29 @@
+/******************************************************************
+ * \file	exercise_1_6_checkEOFValue.c
+ *
+ * \brief 	Program that checks what the value of EOF is.
+ * 
+ * \description	This file is using the stdio library to print
+ *		the value of EOF. 
+ *
+ * \author	Mihail P. (MP)
+ * \date	09.XI.2021 03:24AM
+ * \tag		"The C Programming Language 2nd Edition"
+ * 		by Brian W. Kernighan and Dennis M. Ritchie
+ * 		Chapter 1, Section 1.5
+ * 		page 17, Exercise 1-6
+ ******************************************************************/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	int c;
+	
+	while( c = (getchar() != EOF))
+		printf("%d\n",c);
+	printf("%d\n",c); /* print when the value is 0 */
+	return 0;
+}
+
