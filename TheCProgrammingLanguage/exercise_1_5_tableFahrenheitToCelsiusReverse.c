e179130f0b3fc8e314ca0d2a5734779bafc500b7
MihailP <mihailpascu21@gmail.com>
2021-11-09 03:18:10 +0200

    Removed magic numbers from Exercise 1-5

--- TheCProgrammingLanguage/exercise_1_5_tableFahrenheitToCelsiusReverse.c
+++ TheCProgrammingLanguage/exercise_1_5_tableFahrenheitToCelsiusReverse.c
@@ -19,28 +19,27 @@
 
 #include <stdio.h>
 
+#define LOWER	0	/* lower limit of the table */
+#define UPPER	300	/* upper limit of the table */
+#define STEP	0	/* step size */
+
 /* print Fahrenheit-Celsius table
    for fahr = 300, 280, ..., 0; floating-point version */
 int main(void)
 {
 	float fahr, celsius;
-	int lower, upper, step;
 
-	lower = 0;	/* lower limit of temperature table */
-	upper = 300;	/* upper limit */
-	step = -20;	/* step size */
-
-	fahr = upper;
+	fahr = UPPER;
 
 	/* print table header */
 	printf("Fahrenheit | Celsius\n");
 	printf("-----------|--------\n");
 
-	while (fahr >= lower)
+	while (fahr >= LOWER)
 	{
 		celsius = (5.0/9.0) * (fahr-32.0);
 		printf("%10.0f | %6.1f\n",fahr,celsius);
-		fahr = fahr + step;
+		fahr = fahr + STEP;
 	}
 	return 0;
 }
