04b734c57085be4efc0ae966dc35ee1a3fd8fcfe
Mihail P <mihail.pascu21@gmail.com>
2025-06-30 22:37:17 +0300

    Create my own strlen.

---
+++ CPractice/ex3_implement_strlen.c
@@ -0,0 +1,50 @@
+/**********************************************************************
+ * \file        ex3_implement_strlen.c
+ *
+ * \brief       Implement your own strlen() using pointer arithmetic.
+ * 
+ * \description N.A.
+ * 
+ *
+ * \author      Mihail P. (MP)
+ * \date        30.VI.2025 10:04PM
+ **********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LIMIT 100
+
+int myStrlen(const char* str);
+
+int main(void)
+{
+    char* inputString=(char*) malloc(sizeof(char)*MAX_LIMIT);
+
+    printf("Please enter the input string:");
+    if(fgets(inputString, MAX_LIMIT, stdin) != NULL)
+    {
+        inputString[strcspn(inputString, "\r\n")] = 0;
+        printf("The string %s have length %ld and my function returns %d.\n",inputString,strlen(inputString),myStrlen(inputString)); 
+    }
+    else
+    {
+		printf("Error reading the input.\n");
+    }
+
+    free(inputString);
+    return 0;
+}
+
+int myStrlen(const char* str)
+{
+    int len=0;
+
+    while(*str != '\0')
+    {
+        str++;
+        len++;
+    }
+    return len;
+}
